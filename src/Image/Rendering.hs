0d5c0dfd24ffbc0109e103263bd613e8a239cfe1
Rob Rix <rob.rix@github.com>
2016-01-31 15:49:20 -0500

    Stub in a toPPM function.

--- src/Image/Rendering.hs
+++ src/Image/Rendering.hs
@@ -1,5 +1,6 @@
 module Image.Rendering where
 
+import Data.ByteString hiding (length)
 import Image.Colour
 
 type Sample = Colour
@@ -13,3 +14,6 @@ getWidth = length . getPixels
 getHeight :: Rendering -> Int
 getHeight (Rendering []) = 0
 getHeight (Rendering (column : _)) = length column
+
+toPPM :: Rendering -> ByteString
+toPPM (Rendering pixels) = pack []
