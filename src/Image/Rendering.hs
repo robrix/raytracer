5eea8dfe2802a0c54405d8984ba37f41d260b387
Rob Rix <rob.rix@github.com>
2016-01-31 15:56:24 -0500

    Pack a header.

--- src/Image/Rendering.hs
+++ src/Image/Rendering.hs
@@ -1,6 +1,7 @@
 module Image.Rendering where
 
 import Data.ByteString hiding (length)
+import Data.ByteString.Internal (c2w)
 import Image.Colour
 
 type Sample = Colour
@@ -16,4 +17,5 @@ getHeight (Rendering []) = 0
 getHeight (Rendering (column : _)) = length column
 
 toPPM :: Rendering -> ByteString
-toPPM (Rendering pixels) = pack []
+toPPM r = pack header
+  where header = fmap c2w $ "P6 " ++ show (getWidth r) ++ " " ++ show (getHeight r) ++ " 255\n"
