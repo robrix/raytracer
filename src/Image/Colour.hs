9b5bc00f690737f607b297040546c859474f0e7d
Rob Rix <rob.rix@github.com>
2016-01-31 16:10:20 -0500

    Colour is a Monoid (additively).

--- src/Image/Colour.hs
+++ src/Image/Colour.hs
@@ -4,3 +4,7 @@ data Colour = Colour !Float !Float !Float !Float
 
 clear :: Colour
 clear = Colour 0 0 0 0
+
+instance Monoid Colour where
+  mempty = clear
+  mappend (Colour r1 g1 b1 a1) (Colour r2 g2 b2 a2) = Colour (r1 + r2) (g1 + g2) (b1 + b2) (a1 + a2)
