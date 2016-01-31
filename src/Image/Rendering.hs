5e06d0c0728a6cccff1474f8e001d80d2bef5ed3
Rob Rix <rob.rix@github.com>
2016-01-31 16:36:22 -0500

    Pack the rows.

--- src/Image/Rendering.hs
+++ src/Image/Rendering.hs
@@ -1,7 +1,9 @@
 module Image.Rendering where
 
+import Control.Monad
 import Data.ByteString hiding (length)
 import Data.ByteString.Internal (c2w)
+import Data.Monoid
 import Image.Colour
 
 type Sample = Colour
@@ -17,8 +19,10 @@ getWidth (Rendering []) = 0
 getWidth (Rendering (row : _)) = length row
 
 toPPM :: Rendering -> ByteString
-toPPM r = pack header
+toPPM r = pack header <> pack (join (fmap (join . fmap pixelToWords) (getPixels r)))
   where header = fmap c2w $ "P6 " ++ show (getWidth r) ++ " " ++ show (getHeight r) ++ " 255\n"
+        pixelToWords p = case average p of Colour r g b a -> fmap componentToWord [ r, g, b, a ]
+        componentToWord c = fromIntegral (round (c / 255))
 
 average :: Pixel -> Colour
 average p = case mconcat p of Colour r g b a -> Colour (r / l) (g / l) (b / l) (a / l)
