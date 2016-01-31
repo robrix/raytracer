e7ee9265d3cc0e881797c353a8a5e4735860fdde
Rob Rix <rob.rix@github.com>
2016-01-31 11:09:04 -0500

    Add a zipWith function over Vector.

--- src/Geometry/Vector.hs
+++ src/Geometry/Vector.hs
@@ -1,3 +1,6 @@
 module Geometry.Vector where
 
 data Vector = Vector !Float !Float !Float
+
+zipWith :: (Float -> Float -> Float) -> Vector -> Vector -> Vector
+zipWith f (Vector x1 y1 z1) (Vector x2 y2 z2) = Vector (f x1 x2) (f y1 y2) (f z1 z2)
