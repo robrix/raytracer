7dd2b6be1058f60a4ece4c4d5f41d54477d6c47b
Rob Rix <rob.rix@github.com>
2016-01-31 15:41:22 -0500

    Define a getHeight function.

--- src/Image/Rendering.hs
+++ src/Image/Rendering.hs
@@ -9,3 +9,7 @@ newtype Rendering = Rendering { getPixels :: [[Pixel]] }
 
 getWidth :: Rendering -> Int
 getWidth = length . getPixels
+
+getHeight :: Rendering -> Int
+getHeight (Rendering []) = 0
+getHeight (Rendering (column : _)) = length column
