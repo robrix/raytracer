b0fb9b7285ab9370fa167e64d755cef30a93c20e
Rob Rix <rob.rix@github.com>
2016-01-31 11:16:55 -0500

    Add a Num instance for Vector.

--- src/Geometry/Vector.hs
+++ src/Geometry/Vector.hs
@@ -1,5 +1,7 @@
 module Geometry.Vector where
 
+import Prelude hiding (zipWith)
+
 data Vector = Vector !Float !Float !Float
 
 zipWith :: (Float -> Float -> Float) -> Vector -> Vector -> Vector
@@ -7,3 +9,15 @@ zipWith f (Vector x1 y1 z1) (Vector x2 y2 z2) = Vector (f x1 x2) (f y1 y2) (f z1
 
 magnitude :: Vector -> Float
 magnitude (Vector a b c) = sqrt (a * a + b * b + c * c)
+
+instance Num Vector where
+  (+) = zipWith (+)
+  (*) = zipWith (*)
+  (-) = zipWith (-)
+
+  abs (Vector a b c) = Vector (abs a) (abs b) (abs c)
+
+  signum v@(Vector 0 0 0) = v
+  signum v@(Vector a b c) = Vector (a / r) (b / r) (c / r) where r = magnitude v
+
+  fromInteger i = Vector (fromInteger i) 0 0
