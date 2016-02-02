2d30ad4daf9df14818e77843e93be46138b876e4
Rob Rix <rob.rix@github.com>
2016-02-01 20:59:27 -0500

    Compute the initial rays.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import qualified Data.ByteString as ByteString
+import Geometry.Ray
+import Geometry.Vector
 import Image.Colour
 import Image.Rendering
 import System.Environment
@@ -14,3 +16,10 @@ rendering :: Rendering
 rendering = Rendering $ fmap toRow [0..3]
   where toRow i = fmap (toPixel i) [0..3]
         toPixel r b = [ Colour (r / 3) 0 (b / 3) 1 ]
+
+        width = 800
+        height = 600
+        row y = [ Ray { getLocation = Vector x y 0, getDirection = Vector 0 0 1 } | x <- [-width / 2..width / 2] ]
+        rays = do
+          y <- [-height / 2..height / 2]
+          row y
