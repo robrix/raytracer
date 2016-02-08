b95464bc78561581864e58400adcd0e94be6e06f
Rob Rix <rob.rix@github.com>
2016-02-08 14:44:55 -0500

    Stub in a function to compute the set of intersections between a Ray and a Sphere as a list of Vectors ordered by proximity to the Ray’s origin.

--- src/Geometry/Ray.hs
+++ src/Geometry/Ray.hs
@@ -1,5 +1,9 @@
 module Geometry.Ray where
 
 import Geometry.Vector
+import Geometry.Sphere
 
 data Ray = Ray { getLocation :: !Vector, getDirection :: !Vector }
+
+intersectionsWithSphere :: Ray -> Sphere -> [Vector]
+intersectionsWithSphere _ _ = []
