9cd2ddb18e0bb197532f17e3eb37d8cf620c0258
Rob Rix <rob.rix@github.com>
2016-02-08 14:50:39 -0500

    Implement intersectionsWithSphere by analogy with trace.

--- src/Geometry/Ray.hs
+++ src/Geometry/Ray.hs
@@ -1,5 +1,6 @@
 module Geometry.Ray where
 
+import Data.List (sort)
 import Geometry.Vector
 import Geometry.Sphere
 
@@ -7,4 +8,11 @@ data Ray = Ray { getLocation :: !Vector, getDirection :: !Vector }
 
 -- | Compute the set of intersections between a Ray and a Sphere as a list of Vectors in increasing order of distance.
 intersectionsWithSphere :: Ray -> Sphere -> [Vector]
-intersectionsWithSphere _ _ = []
+intersectionsWithSphere (Ray o l) (Sphere c r) = if under < 0 then [] else atDistance <$> sort [ d1, d2 ]
+  where d1 = negate dotted + sqrt under
+        d2 = negate dotted - sqrt under
+        translated = o - c
+        under = dotted * dotted - offset * offset + r * r
+        dotted = l `dot` translated
+        offset = magnitude translated
+        atDistance d = l + o * fromScalar d
