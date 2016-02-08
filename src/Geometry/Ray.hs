27988e26b60e5ba37e43c3868d6cce1f94cf910e
Rob Rix <rob.rix@github.com>
2016-02-08 14:45:46 -0500

    Document the intersections function.

--- src/Geometry/Ray.hs
+++ src/Geometry/Ray.hs
@@ -5,5 +5,6 @@ import Geometry.Sphere
 
 data Ray = Ray { getLocation :: !Vector, getDirection :: !Vector }
 
+-- | Compute the set of intersections between a Ray and a Sphere as a list of Vectors in increasing order of distance.
 intersectionsWithSphere :: Ray -> Sphere -> [Vector]
 intersectionsWithSphere _ _ = []
