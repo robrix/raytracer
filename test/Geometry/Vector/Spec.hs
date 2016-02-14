bd22af5cd783c766446674b959f583ca109ca5b0
Rob Rix <rob.rix@github.com>
2016-02-14 16:27:18 -0500

    Test the negativity of opposed vectors’ dot products for arbitrary Vectors.

--- test/Geometry/Vector/Spec.hs
+++ test/Geometry/Vector/Spec.hs
@@ -14,5 +14,7 @@ spec = do
     it "is zero for orthogonal vectors" $
       (Vector 0 0 1 `dot` Vector 1 0 0) `shouldBe` 0
 
-    it "is negative for opposed vectors" $
-      (Vector 1 0 0 `dot` Vector (negate 1) 0 0) `shouldBe` -1
+    prop "is negative for opposed vectors" $
+      \ v -> (v `dot` (v * fromScalar (negate 1))) `shouldSatisfy` isNegative
+
+  where isNegative f = f < 0 || isNegativeZero f
