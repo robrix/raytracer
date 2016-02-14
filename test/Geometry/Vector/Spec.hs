6fde7b8bd600796cff8eea278cd46f538380a71c
Rob Rix <rob.rix@github.com>
2016-02-14 16:21:37 -0500

    Test that `dot` is negative for opposed vectors.

--- test/Geometry/Vector/Spec.hs
+++ test/Geometry/Vector/Spec.hs
@@ -8,3 +8,6 @@ spec = do
   describe "dot" $ do
     it "is zero for orthogonal vectors" $
       (Vector 0 0 1 `dot` Vector 1 0 0) `shouldBe` 0
+
+    it "is negative for opposed vectors" $
+      (Vector 1 0 0 `dot` Vector (negate 1) 0 0) `shouldBe` -1
