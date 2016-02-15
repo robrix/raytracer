f5cd4167b2934b65b4a2b421bbb0bb84ceae37d3
Rob Rix <rob.rix@github.com>
2016-02-14 23:42:33 -0500

    Test opposed dot products using a ridiculously complicated process & an obscenely large value of epsilon.

--- test/Geometry/Vector/Spec.hs
+++ test/Geometry/Vector/Spec.hs
@@ -17,10 +17,14 @@ spec = do
     prop "is the square of the length of a vector when applied to that vector" $
       \ v -> sqrt (v `dot` v) `shouldBe` magnitude v
 
-    prop "is negative for opposed vectors" $
-      \ v -> (v `dot` (v * fromScalar (negate 1))) `shouldSatisfy` isNegative
+    prop "is the negation of the square of the length of a vector when applied to that vector’s inverse" $
+      \ v -> (v `dot` (v * fromScalar (negate 1))) `shouldSatisfy` isWithinEpsilon 1 (negate (magnitude v * magnitude v))
 
     prop "is commutative" $
       \ a b -> (a `dot` b) `shouldBe` (b `dot` a)
 
-  where isNegative f = f < 0 || isNegativeZero f
+  where isWithinEpsilon _ actual expected | actual == expected = True
+        isWithinEpsilon epsilon actual expected = let diff = abs (actual - expected)
+                                                      minNormal = fromIntegral (fst (floatRange actual))
+                                                      maxValue = fromIntegral (snd (floatRange actual)) in
+                                                      if actual == 0 || expected == 0 || diff < minNormal then diff < epsilon * minNormal else diff / min (abs actual + abs expected) maxValue < epsilon
