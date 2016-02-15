d73728048f798d98ec3052680c4f0d27f619f657
Rob Rix <rob.rix@github.com>
2016-02-15 01:58:40 -0500

    Rename l to direction.

--- src/Geometry/Ray.hs
+++ src/Geometry/Ray.hs
@@ -11,11 +11,11 @@ data Intersection = Intersection { getGlobalCoordinates :: !Vector, getNormal ::
 
 -- | Compute the set of intersections between a Ray and a Sphere as a list of Intersections in increasing order of distance.
 intersectionsWithSphere :: Ray -> Sphere -> [Intersection]
-intersectionsWithSphere (Ray o l) (Sphere c r) = if discriminant < 0 then [] else atDistance <$> sort [ d1, d2 ]
+intersectionsWithSphere (Ray o direction) (Sphere c r) = if discriminant < 0 then [] else atDistance <$> sort [ d1, d2 ]
   where d1 = negate dotted + sqrt discriminant
         d2 = negate dotted - sqrt discriminant
         translated = o - c
         discriminant = dotted * dotted - offset * offset + r * r
-        dotted = l `dot` translated
+        dotted = direction `dot` translated
         offset = magnitude translated
-        atDistance d = Intersection (l + o * fromScalar d) ((l + o * fromScalar d) - c / fromScalar r)
+        atDistance d = Intersection (direction + o * fromScalar d) ((direction + o * fromScalar d) - c / fromScalar r)
