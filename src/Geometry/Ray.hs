17857544acaf2153c376acc0acd989ba1a6a2529
Rob Rix <rob.rix@github.com>
2016-02-15 00:06:32 -0500

    Rename the discriminant variable.

--- src/Geometry/Ray.hs
+++ src/Geometry/Ray.hs
@@ -11,11 +11,11 @@ data Intersection = Intersection { getGlobalCoordinates :: !Vector, getNormal ::
 
 -- | Compute the set of intersections between a Ray and a Sphere as a list of Intersections in increasing order of distance.
 intersectionsWithSphere :: Ray -> Sphere -> [Intersection]
-intersectionsWithSphere (Ray o l) (Sphere c r) = if under < 0 then [] else atDistance <$> sort [ d1, d2 ]
-  where d1 = negate dotted + sqrt under
-        d2 = negate dotted - sqrt under
+intersectionsWithSphere (Ray o l) (Sphere c r) = if discriminant < 0 then [] else atDistance <$> sort [ d1, d2 ]
+  where d1 = negate dotted + sqrt discriminant
+        d2 = negate dotted - sqrt discriminant
         translated = o - c
-        under = dotted * dotted - offset * offset + r * r
+        discriminant = dotted * dotted - offset * offset + r * r
         dotted = l `dot` translated
         offset = magnitude translated
         atDistance d = Intersection (l + o * fromScalar d) ((l + o * fromScalar d) - c / fromScalar r)
