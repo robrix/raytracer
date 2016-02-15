6bf6f013ee4dc143687b0f72bf6f05fd939f3478
Rob Rix <rob.rix@github.com>
2016-02-15 02:40:05 -0500

    Correct the computation of the distance terms.

--- src/Geometry/Ray.hs
+++ src/Geometry/Ray.hs
@@ -12,8 +12,8 @@ data Intersection = Intersection { getGlobalCoordinates :: !Vector, getNormal ::
 -- | Compute the set of intersections between a Ray and a Sphere as a list of Intersections in increasing order of distance.
 intersectionsWithSphere :: Ray -> Sphere -> [Intersection]
 intersectionsWithSphere (Ray origin direction) (Sphere centre radius) = if discriminant < 0 then [] else atDistance <$> sort [ t0, t1 ]
-  where t0 = negate t1
-        t1 = b + sqrt discriminant / 2
+  where t0 = -b - sqrt discriminant / 2
+        t1 = -b + sqrt discriminant / 2
         b = 2 * case direction * translated of Vector dx dy dz -> dx + dy + dz
         c = case translated of Vector tx ty tz -> tx ** 2 + ty ** 2 + tz ** 2 - radius ** 2
         translated = origin - centre
