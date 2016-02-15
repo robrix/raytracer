db23e3b06053880976c47e924fa193607f0b908a
Rob Rix <rob.rix@github.com>
2016-02-15 01:59:49 -0500

    Rename r to radius.

--- src/Geometry/Ray.hs
+++ src/Geometry/Ray.hs
@@ -11,11 +11,11 @@ data Intersection = Intersection { getGlobalCoordinates :: !Vector, getNormal ::
 
 -- | Compute the set of intersections between a Ray and a Sphere as a list of Intersections in increasing order of distance.
 intersectionsWithSphere :: Ray -> Sphere -> [Intersection]
-intersectionsWithSphere (Ray origin direction) (Sphere centre r) = if discriminant < 0 then [] else atDistance <$> sort [ d1, d2 ]
+intersectionsWithSphere (Ray origin direction) (Sphere centre radius) = if discriminant < 0 then [] else atDistance <$> sort [ d1, d2 ]
   where d1 = negate dotted + sqrt discriminant
         d2 = negate dotted - sqrt discriminant
         translated = origin - centre
-        discriminant = dotted * dotted - offset * offset + r * r
+        discriminant = dotted * dotted - offset * offset + radius * radius
         dotted = direction `dot` translated
         offset = magnitude translated
-        atDistance d = Intersection (direction + origin * fromScalar d) ((direction + origin * fromScalar d) - centre / fromScalar r)
+        atDistance d = Intersection (direction + origin * fromScalar d) ((direction + origin * fromScalar d) - centre / fromScalar radius)
