22b1da405ffb95daf0b294faeca3ac646e02846f
Rob Rix <rob.rix@github.com>
2016-02-15 02:51:45 -0500

    Filter out negative intersection distances.

--- src/Geometry/Ray.hs
+++ src/Geometry/Ray.hs
@@ -11,7 +11,7 @@ data Intersection = Intersection { getGlobalCoordinates :: !Vector, getNormal ::
 
 -- | Compute the set of intersections between a Ray and a Sphere as a list of Intersections in increasing order of distance.
 intersectionsWithSphere :: Ray -> Sphere -> [Intersection]
-intersectionsWithSphere (Ray origin direction) (Sphere centre radius) = if discriminant < 0 then [] else atDistance <$> sort [ t0, t1 ]
+intersectionsWithSphere (Ray origin direction) (Sphere centre radius) = if discriminant < 0 then [] else atDistance <$> filter (> 0) (sort [ t0, t1 ])
   where t0 = -b - sqrt discriminant / 2
         t1 = -b + sqrt discriminant / 2
         b = 2 * case direction * translated of Vector dx dy dz -> dx + dy + dz
