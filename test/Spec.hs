5e12f860a98b8416a214174a4b8d966237d0f668
Rob Rix <rob.rix@me.com>
2018-06-23 09:46:46 -0400

    :fire: Geometry.Vector.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,6 @@
 import Test.Hspec
 import Geometry.Ray.Spec
-import Geometry.Vector.Spec
 
 main :: IO ()
 main = hspec $ do
   describe "Geometry.Ray" Geometry.Ray.Spec.spec
-  describe "Geometry.Vector" Geometry.Vector.Spec.spec
