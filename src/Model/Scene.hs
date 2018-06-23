512fc3ddd6449002644d6283febfe0af452151fe
Rob Rix <rob.rix@me.com>
2018-06-23 10:03:34 -0400

    Reformat the colour.

--- src/Model/Scene.hs
+++ src/Model/Scene.hs
@@ -15,8 +15,11 @@ trace :: RealFloat a => Int -> Scene a -> Ray a -> Sample a
 trace 0 _ _ = clear
 trace _ (Scene sphere) ray@(Ray _ d) = case intersectionsWithSphere ray sphere of
   [] -> clear
-  (Intersection _ normal : _) ->
-    Colour (P (V4 (min (abs (_x `dot` normal)) 255) (min (abs (_y `dot` normal)) 255) (min (abs (_z `dot` normal)) 255) (d `dot` normal)))
+  (Intersection _ normal : _) -> Colour (P (V4
+    (min (abs (_x `dot` normal)) 255)
+    (min (abs (_y `dot` normal)) 255)
+    (min (abs (_z `dot` normal)) 255)
+    (d `dot` normal)))
   where _x = V3 1 0 0
         _y = V3 0 1 0
         _z = V3 0 0 1
