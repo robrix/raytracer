a7bdf8fa97ae327e146483147bf19f21a3396092
Rob Rix <rob.rix@me.com>
2018-06-23 10:22:15 -0400

    :fire: Colour.

--- src/Image/Rendering.hs
+++ src/Image/Rendering.hs
@@ -3,10 +3,11 @@ module Image.Rendering where
 import Control.Monad
 import Data.ByteString hiding (length)
 import Data.ByteString.Internal (c2w)
-import Image.Colour
+import Linear.Affine
+import Linear.V4
 import Linear.Vector
 
-type Sample a = Colour a
+type Sample a = Point V4 a
 type Pixel a = [Sample a]
 
 newtype Rendering a = Rendering { getPixels :: [[Pixel a]] }
@@ -21,12 +22,13 @@ getWidth (Rendering (row : _)) = length row
 toPPM :: RealFrac a => Rendering a -> ByteString
 toPPM r = pack header <> pack (join (fmap (join . fmap pixelToWords) (getPixels r)))
   where header = fmap c2w $ "P6 " ++ show (getWidth r) ++ " " ++ show (getHeight r) ++ " 255\n"
-        pixelToWords p = let Colour (P (V4 r g b _)) = average p in fmap componentToWord [ r, g, b ]
+        pixelToWords p = let P (V4 r g b _) = average p in fmap componentToWord [ r, g, b ]
         componentToWord c = max 0 $ min 255 $ round (c * 255)
 
-average :: Fractional a => Pixel a -> Colour a
-average p = case mconcat p of Colour (P (V4 r g b a)) -> Colour (P (V4 (r / l) (g / l) (b / l) (a / l)))
-  where l = fromIntegral (length p)
+average :: Fractional a => Pixel a -> Sample a
+average p = P (V4 (r / l) (g / l) (b / l) (a / l))
+  where P (V4 r g b a) = getAdd $ foldMap Add p
+        l = fromIntegral (length p)
 
 newtype Add f a = Add { getAdd :: f a }
 
--- src/Model/Scene.hs
+++ src/Model/Scene.hs
@@ -4,22 +4,24 @@ import Control.Parallel.Strategies hiding (dot)
 import qualified Data.ByteString as ByteString
 import Geometry.Ray
 import Geometry.Sphere
-import Image.Colour
 import Image.Rendering
+import Linear.Affine
 import Linear.Metric
 import Linear.V3
+import Linear.V4
+import Linear.Vector
 
 data Scene a = Scene (Sphere a)
 
 trace :: RealFloat a => Int -> Scene a -> Ray a -> Sample a
-trace 0 _ _ = clear
+trace 0 _ _ = zero
 trace _ (Scene sphere) ray@(Ray _ d) = case intersectionsWithSphere ray sphere of
-  [] -> clear
-  (Intersection _ normal : _) -> Colour (P (V4
+  [] -> zero
+  Intersection _ normal : _ -> P (V4
     (min (abs (_x `dot` normal)) 255)
     (min (abs (_y `dot` normal)) 255)
     (min (abs (_z `dot` normal)) 255)
-    (d `dot` normal)))
+    (d `dot` normal))
   where _x = V3 1 0 0
         _y = V3 0 1 0
         _z = V3 0 0 1
