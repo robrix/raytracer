11be2d25c37a83d3103c588364f3256339eb02fb
Rob Rix <rob.rix@me.com>
2018-06-23 10:36:08 -0400

    Avoid some cases.

--- src/Geometry/Ray.hs
+++ src/Geometry/Ray.hs
@@ -23,8 +23,10 @@ intersectionsWithSphere :: RealFloat a => Ray a -> Sphere a -> [Intersection a]
 intersectionsWithSphere (Ray origin direction) (Sphere centre radius) = if discriminant < 0 then [] else atDistance <$> filter (> 0) (sort [ t0, t1 ])
   where t0 = (-b - sqrt discriminant) / 2
         t1 = (-b + sqrt discriminant) / 2
-        b = 2 * case P direction * translated of P (V3 dx dy dz) -> dx + dy + dz
-        c = case translated of P (V3 tx ty tz) -> tx ** 2 + ty ** 2 + tz ** 2 - radius ** 2
-        translated = origin - centre
+        P (V3 dx dy dz) = P direction * translated
+        b = 2 * dx + dy + dz
+        c = tx ** 2 + ty ** 2 + tz ** 2 - radius ** 2
+        translated@(P (V3 tx ty tz)) = origin - centre
         discriminant = b ** 2 - 4 * c
-        atDistance d = let intersection = origin + P direction * P (V3 d d d) in Intersection intersection (unP (intersection - centre) / V3 radius radius radius)
+        atDistance d = Intersection intersection (unP (intersection - centre) / V3 radius radius radius)
+          where intersection = origin + P direction * P (V3 d d d)
