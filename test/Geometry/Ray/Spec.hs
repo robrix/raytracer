006bde135ded351a2e44c4c90a4d9a845e2cd867
Rob Rix <rob.rix@me.com>
2018-06-23 09:44:32 -0400

    Fix up the Ray spec.

--- test/Geometry/Ray/Spec.hs
+++ test/Geometry/Ray/Spec.hs
@@ -2,19 +2,22 @@ module Geometry.Ray.Spec where
 
 import Geometry.Ray
 import Geometry.Sphere
-import Geometry.Vector
+import Linear.Affine
+import Linear.V3
 import Test.Hspec
-import Test.Hspec.QuickCheck
-import Test.QuickCheck
 
 spec :: Spec
 spec = do
   describe "intersectionsWithSphere" $ do
     it "is empty when directed away from the sphere" $
-      intersectionsWithSphere (Ray (Vector 0 0 100) (Vector 0 0 1)) (Sphere (Vector 0 0 0) 50) `shouldBe` []
+      intersectionsWithSphere (Ray (P (V3 0 0 100)) (V3 0 0 1)) (Sphere (P (V3 0 0 0)) (50 :: Float)) `shouldBe` []
 
     it "is empty when it misses the sphere" $
-      intersectionsWithSphere (Ray (Vector 100 100 100) (Vector 0 0 (-1))) (Sphere (Vector 0 0 0) 50) `shouldBe` []
+      intersectionsWithSphere (Ray (P (V3 100 100 100)) (V3 0 0 (-1))) (Sphere (P (V3 0 0 0)) (50 :: Float)) `shouldBe` []
 
     it "produces intersections for Ray collisions" $
-      intersectionsWithSphere (Ray (Vector 0 0 100) (Vector 0 0 (-1))) (Sphere (Vector 0 0 0) 50) `shouldBe` [ Intersection (Vector 0 0 50) (Vector 0 0 1), Intersection (Vector 0 0 (-50)) (Vector 0 0 (-1)) ]
+      intersectionsWithSphere (Ray (P (V3 0 0 100)) (V3 0 0 (-1))) (Sphere (P (V3 0 0 0)) 50)
+      `shouldBe`
+      [ Intersection (P (V3 0 0   50))  (V3 0 0   1)
+      , Intersection (P (V3 0 0 (-50))) (V3 0 0 (-1 :: Float))
+      ]
