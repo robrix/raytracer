d1036cabbb5a36ca5ae4b383ed067310900d111a
Rob Rix <rob.rix@me.com>
2018-06-23 10:25:25 -0400

    Rename the Rendering field selector.

--- src/Image/Rendering.hs
+++ src/Image/Rendering.hs
@@ -10,17 +10,17 @@ import Linear.Vector
 type Sample a = Point V4 a
 type Pixel a = [Sample a]
 
-newtype Rendering a = Rendering { getPixels :: [[Pixel a]] }
+newtype Rendering a = Rendering { pixels :: [[Pixel a]] }
 
 getHeight :: Rendering a -> Int
-getHeight = length . getPixels
+getHeight = length . pixels
 
 getWidth :: Rendering a -> Int
 getWidth (Rendering []) = 0
 getWidth (Rendering (row : _)) = length row
 
 toPPM :: RealFrac a => Rendering a -> ByteString
-toPPM r = pack header <> pack (join (fmap (join . fmap pixelToWords) (getPixels r)))
+toPPM r = pack header <> pack (join (fmap (join . fmap pixelToWords) (pixels r)))
   where header = c2w <$> "P6 " ++ show (getWidth r) ++ " " ++ show (getHeight r) ++ " 255\n"
         pixelToWords p = let P (V4 r g b _) = average p in map componentToWord [ r, g, b ]
         componentToWord c = max 0 $ min 255 $ round (c * 255)
