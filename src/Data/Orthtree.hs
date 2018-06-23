7b96d2da8f242b61faf7bd08d81c06fe45f6abda
Rob Rix <rob.rix@me.com>
2018-06-23 11:18:27 -0400

    :fire: Orthtree.

--- src/Data/Orthtree.hs
+++
@@ -1,7 +0,0 @@
-module Data.Orthtree where
-
-data Orthtree i a = Orthtree { getDimensions :: !Int, getOrthants :: !(Orthant i a) }
-data Orthant i a = Orthant !(i (a, Orthant i a)) | Nil
-
-empty :: Int -> Orthtree i a
-empty dimensions = Orthtree dimensions Nil
