d7e7389c68b641b4ed44122477295f809f9f9632
Rob Rix <rob.rix@me.com>
2018-06-24 07:37:09 -0400

    Add monadic constructors.

--- src/Probability/Distribution.hs
+++ src/Probability/Distribution.hs
@@ -3,3 +3,6 @@ module Probability.Distribution where
 
 data Distribution num a where
   StdRandom :: Distribution num num
+
+  Pure :: a -> Distribution num a
+  Then :: Distribution num b -> (b -> Distribution num a) -> Distribution num a
