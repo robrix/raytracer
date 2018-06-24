8ffd48622f92d1770b0f2b82fcf656bbc632d5b4
Rob Rix <rob.rix@me.com>
2018-06-24 07:41:32 -0400

    Define an Applicative instance for Distribution.

--- src/Probability/Distribution.hs
+++ src/Probability/Distribution.hs
@@ -13,3 +13,10 @@ instance Functor (Distribution num) where
   fmap f (Pure a) = Pure (f a)
   fmap f (r :>>= k) = r :>>= fmap f . k
   fmap f a = a :>>= Pure . f
+
+instance Applicative (Distribution num) where
+  pure = Pure
+
+  Pure f     <*> a = fmap f a
+  (r :>>= k) <*> a = r :>>= ((<*> a) . k)
+  f          <*> a = f :>>= (flip fmap a)
