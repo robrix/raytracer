b5e05cb7f8802fe572b78e5cf76233e372346a9f
Rob Rix <rob.rix@me.com>
2018-06-24 14:17:27 -0400

    Render twice as many samples.

--- app/Main.hs
+++ app/Main.hs
@@ -10,4 +10,4 @@ import System.Environment
 main :: IO ()
 main = do
   [path] <- getArgs
-  renderToFile (V2 800 600) 480000 path (Scene (Sphere (P (V3 0 0 10)) (250 :: Double)))
+  renderToFile (V2 800 600) (800 * 600 * 2) path (Scene (Sphere (P (V3 0 0 10)) (250 :: Double)))
