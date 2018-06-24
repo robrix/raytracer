6f7d39084e702e069ef3c8f3eb4cdb6660b66040
Rob Rix <rob.rix@me.com>
2018-06-24 07:39:15 -0400

    Rename the Then constructor.

--- src/Probability/Distribution.hs
+++ src/Probability/Distribution.hs
@@ -5,4 +5,4 @@ data Distribution num a where
   StdRandom :: Distribution num num
 
   Pure :: a -> Distribution num a
-  Then :: Distribution num b -> (b -> Distribution num a) -> Distribution num a
+  (:>>=) :: Distribution num b -> (b -> Distribution num a) -> Distribution num a
