75e18ad3bed65047424208634bdf91d7fda68095
Rob Rix <rob.rix@me.com>
2018-06-30 20:48:58 -0400

    Add a small white light to the scene.

--- app/Main.hs
+++ app/Main.hs
@@ -14,4 +14,5 @@ main = do
   renderToFile (V2 800 600) (800 * 600 * 2) path scene
   where scene = Scene (Light (P (V3 0 300 0)) (P (V3 1 1 1)) 20)
           [ Model (Sphere (P (V3 0 0 10)) (250 :: Double)) (P (V4 0.25 0.25 0.25 1)) (P (V4 1 0 1 1))
+          , Model (Sphere (P (V3 0 300 0)) 20) (P (V4 1 1 1 1)) (P (V4 0 0 0 0))
           ]
