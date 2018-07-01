b0a75b85d12a218ec8af7c61f7a36c1b08598596
Rob Rix <rob.rix@me.com>
2018-07-01 18:02:37 -0400

    Fix up the main file.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Geometry.Sphere
+import qualified Geometry.Sphere as G
 import Linear.Affine
 import Linear.V2
 import Linear.V3
@@ -12,8 +12,8 @@ main = do
   [path] <- getArgs
   renderToFile (V2 800 600) (800 * 600 * 2) path scene
   where scene = Scene
-          [ Model (Sphere (P (V3 0 0 10)) (250 :: Double)) (P (V3 0 0 0)) (P (V3 1 1 1))
-          , Model (Sphere (P (V3 0 350 0)) 50) (P (V3 1 0 0)) (P (V3 0 0 0))
-          , Model (Sphere (P (V3 350 0 0)) 50) (P (V3 0 1 0)) (P (V3 0 0 0))
-          , Model (Sphere (P (V3 0 0 (-350))) 50) (P (V3 0 0 1)) (P (V3 0 0 0))
+          [ Model (Sphere (G.Sphere (P (V3 0 0 10)) (250 :: Double))) (P (V3 0 0 0)) (P (V3 1 1 1))
+          , Model (Sphere (G.Sphere (P (V3 0 350 0)) 50)) (P (V3 1 0 0)) (P (V3 0 0 0))
+          , Model (Sphere (G.Sphere (P (V3 350 0 0)) 50)) (P (V3 0 1 0)) (P (V3 0 0 0))
+          , Model (Sphere (G.Sphere (P (V3 0 0 (-350))) 50)) (P (V3 0 0 1)) (P (V3 0 0 0))
           ]
