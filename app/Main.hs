a71929474a3cf2e02e5d2908284d0e94603e3e85
Rob Rix <rob.rix@me.com>
2018-07-01 18:12:34 -0400

    Use synonyms for the colours.

--- app/Main.hs
+++ app/Main.hs
@@ -14,10 +14,10 @@ main = do
   [path] <- getArgs
   renderToFile (V2 800 600) (800 * 600 * 2) path scene
   where scene = Scene
-          [ Model (Sphere (G.Sphere (P (V3 0 0 10)) (250 :: Double))) (P (V3 0 0 0)) (P (V3 1 1 1))
-          , Model (Sphere (G.Sphere (P (V3 0 350 0)) 50)) (P (V3 1 0 0)) (P (V3 0 0 0))
-          , Model (Sphere (G.Sphere (P (V3 350 0 0)) 50)) (P (V3 0 1 0)) (P (V3 0 0 0))
-          , Model (Sphere (G.Sphere (P (V3 0 0 (-350))) 50)) (P (V3 0 0 1)) (P (V3 0 0 0))
+          [ Model (Sphere (G.Sphere (P (V3 0 0 10)) (250 :: Double))) black white
+          , Model (Sphere (G.Sphere (P (V3 0 350 0)) 50)) red black
+          , Model (Sphere (G.Sphere (P (V3 350 0 0)) 50)) green black
+          , Model (Sphere (G.Sphere (P (V3 0 0 (-350))) 50)) blue black
           , top
           , bottom
           , left
@@ -25,9 +25,15 @@ main = do
           , back
           , front
           ]
-        top = Model (Plane (G.Plane (P (V3 0 500 0)) (-(unit _y)))) zero (P (V3 1 1 1))
-        bottom = Model (Plane (G.Plane (P (V3 0 (-500) 0)) (unit _y))) zero (P (V3 1 1 1))
-        left = Model (Plane (G.Plane (P (V3 500 0 0)) (-(unit _x)))) zero (P (V3 1 1 1))
-        right = Model (Plane (G.Plane (P (V3 (-500) 0 0)) (unit _x))) zero (P (V3 1 1 1))
-        back = Model (Plane (G.Plane (P (V3 0 0 500)) (-(unit _z)))) zero (P (V3 1 1 1))
-        front = Model (Plane (G.Plane (P (V3 0 0 (-500))) (unit _z))) zero (P (V3 1 1 1))
+        top = Model (Plane (G.Plane (P (V3 0 500 0)) (-(unit _y)))) black white
+        bottom = Model (Plane (G.Plane (P (V3 0 (-500) 0)) (unit _y))) black white
+        left = Model (Plane (G.Plane (P (V3 500 0 0)) (-(unit _x)))) black white
+        right = Model (Plane (G.Plane (P (V3 (-500) 0 0)) (unit _x))) black white
+        back = Model (Plane (G.Plane (P (V3 0 0 500)) (-(unit _z)))) black white
+        front = Model (Plane (G.Plane (P (V3 0 0 (-500))) (unit _z))) black white
+
+        black = zero
+        white = P (V3 1 1 1)
+        red = P (V3 1 0 0)
+        green = P (V3 0 1 0)
+        blue = P (V3 0 0 1)
