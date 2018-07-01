c52122d1a5affcd0220cfcd402aad03e36b5632a
Rob Rix <rob.rix@me.com>
2018-07-01 17:46:56 -0400

    Move ray/sphere intersections into Sphere.

--- src/Geometry/Ray.hs
+++ src/Geometry/Ray.hs
@@ -1,13 +1,8 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 module Geometry.Ray where
 
-import Control.Applicative ((<**>))
-import Geometry.Sphere
 import Linear.Affine
-import Linear.Epsilon
-import Linear.Metric (norm, normalize)
 import Linear.V3
-import Linear.Vector ((^*))
 
 data Ray a = Ray
   { origin    :: !(Point V3 a)
@@ -21,13 +16,3 @@ data Intersection a = Intersection
   , normal   :: !(V3 a)
   }
   deriving (Eq, Ord, Show)
-
--- | Compute the set of intersections between a Ray and a Sphere as a list of Intersections in increasing order of distance.
-intersectionsWithSphere :: (Epsilon a, RealFloat a) => Ray a -> Sphere a -> [Intersection a]
-intersectionsWithSphere (Ray origin direction) (Sphere centre radius) = if discriminant < 0 then [] else atDistance <$> filter (> 0) ts
-  where ts = [-b] <**> [(+), (-)] <*> [sqrt discriminant]
-        b = sum (direction * unP translated)
-        translated = origin - centre
-        discriminant = b ** 2 - norm (unP translated) ** 2 + radius ** 2
-        atDistance d = Intersection d intersection (normalize (unP (intersection - centre)))
-          where intersection = origin + P direction ^* d
--- src/Geometry/Sphere.hs
+++ src/Geometry/Sphere.hs
@@ -1,9 +1,24 @@
 module Geometry.Sphere where
 
+import Control.Applicative ((<**>))
+import Geometry.Ray
 import Linear.Affine
+import Linear.Epsilon
+import Linear.Metric (norm, normalize)
 import Linear.V3
+import Linear.Vector ((^*))
 
 data Sphere a = Sphere
   { sphereCentre :: !(Point V3 a)
   , sphereRadius :: !a
   }
+
+-- | Compute the set of intersections between a Ray and a Sphere as a list of Intersections in increasing order of distance.
+intersectionsWithSphere :: (Epsilon a, RealFloat a) => Ray a -> Sphere a -> [Intersection a]
+intersectionsWithSphere (Ray origin direction) (Sphere centre radius) = if discriminant < 0 then [] else atDistance <$> filter (> 0) ts
+  where ts = [-b] <**> [(+), (-)] <*> [sqrt discriminant]
+        b = sum (direction * unP translated)
+        translated = origin - centre
+        discriminant = b ** 2 - norm (unP translated) ** 2 + radius ** 2
+        atDistance d = Intersection d intersection (normalize (unP (intersection - centre)))
+          where intersection = origin + P direction ^* d
