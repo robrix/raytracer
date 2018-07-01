d2c9a40acd03a23eaabe3c5dba29f79414384669
Rob Rix <rob.rix@me.com>
2018-07-01 10:11:30 -0400

    Don’t emit light from the main sphere.

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,7 @@ main = do
   [path] <- getArgs
   renderToFile (V2 800 600) (800 * 600 * 2) path scene
   where scene = Scene
-          [ Model (Sphere (P (V3 0 0 10)) (250 :: Double)) (P (V3 0.1 0.1 0.1)) (P (V3 0.5 0.5 0.5))
+          [ Model (Sphere (P (V3 0 0 10)) (250 :: Double)) (P (V3 0 0 0)) (P (V3 1 1 1))
           , Model (Sphere (P (V3 0 350 0)) 50) (P (V3 1 0 0)) (P (V3 0 0 0))
           , Model (Sphere (P (V3 350 0 0)) 50) (P (V3 0 1 0)) (P (V3 0 0 0))
           , Model (Sphere (P (V3 0 0 350)) 50) (P (V3 0 0 1)) (P (V3 0 0 0))
