0d08fc970e7c434f82bdfafced65cef71440de80
Rob Rix <rob.rix@me.com>
2018-07-01 17:59:44 -0400

    Define ray-plane intersections.

--- src/Geometry/Plane.hs
+++ src/Geometry/Plane.hs
@@ -1,9 +1,23 @@
 module Geometry.Plane where
 
+import Geometry.Ray
 import Linear.Affine
+import Linear.Epsilon
+import Linear.Metric (dot)
 import Linear.V3
+import Linear.Vector ((^*))
 
 data Plane a = Plane
   { origin :: !(Point V3 a)
   , normal :: !(V3 a)
   }
+
+intersections :: (Epsilon a, RealFloat a) => Plane a -> Ray a -> [Intersection a]
+intersections (Plane centre normal) (Ray origin direction)
+  | denom <= 0 = []
+  | t < 0      = []
+  | otherwise  = [Intersection t intersection normal]
+  where denom = normal `dot` direction
+        offset = centre - origin
+        t = unP offset `dot` normal / denom
+        intersection = origin + P direction ^* t
