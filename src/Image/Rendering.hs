551db8e0aca5dad6b0a5d5125cedcaa26f78c9d6
Rob Rix <rob.rix@me.com>
2018-07-01 23:13:18 -0400

    :fire: average.

--- src/Image/Rendering.hs
+++ src/Image/Rendering.hs
@@ -36,9 +36,6 @@ toPPM depth r = header <> encodePixels (pixels r)
           Depth16 -> foldMap (B.word16BE . max 0 . min 65535 . round) . view _xyz . (* 65535)
         size@(V2 w h) = renderingSize r
 
-average :: Fractional a => Pixel a -> Sample a
-average (Pixel p) = getAdd (foldMap Add p) ^/ fromIntegral (length p)
-
 newtype Add f a = Add { getAdd :: f a }
 
 instance (Additive f, Num a) => Semigroup (Add f a) where
