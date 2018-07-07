48e78e865f7646d977ecc6b3030a3268f41c3efd
Rob Rix <rob.rix@me.com>
2018-07-07 14:39:25 -0400

    Give the intersections between geometry and a ray.

--- src/Geometry.hs
+++ src/Geometry.hs
@@ -1,7 +1,10 @@
 module Geometry where
 
+import Geometry.Ray
 import Linear.Affine
+import Linear.Epsilon
 import Linear.V3
 
 class Geometry g where
   origin :: g a -> Point V3 a
+  intersections :: (Epsilon a, RealFloat a) => g a -> Ray a -> [Intersection a]
