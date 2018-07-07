9f46ceae1e88cc462b8fcb283c75ffbd1f81d849
Rob Rix <rob.rix@me.com>
2018-07-07 14:40:15 -0400

    Define a SomeGeometry type existentially wrapping geometry.

--- src/Geometry.hs
+++ src/Geometry.hs
@@ -1,10 +1,18 @@
+{-# LANGUAGE GADTs #-}
 module Geometry where
 
-import Geometry.Ray
-import Linear.Affine
+import Geometry.Ray (Ray, Intersection)
+import Linear.Affine (Point)
 import Linear.Epsilon
 import Linear.V3
 
 class Geometry g where
   origin :: g a -> Point V3 a
   intersections :: (Epsilon a, RealFloat a) => g a -> Ray a -> [Intersection a]
+
+data SomeGeometry a where
+  SomeGeometry :: Geometry g => g a -> SomeGeometry a
+
+instance Geometry SomeGeometry where
+  origin (SomeGeometry g) = origin g
+  intersections (SomeGeometry g) = intersections g
