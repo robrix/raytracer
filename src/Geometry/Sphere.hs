654c6b35f25c52346a16e89f36ec7a189013b3dc
Rob Rix <rob.rix@me.com>
2018-07-08 12:13:59 -0400

    Don’t overload the name intersections for Sphere.

--- src/Geometry/Sphere.hs
+++ src/Geometry/Sphere.hs
@@ -4,7 +4,6 @@ import Control.Applicative ((<**>))
 import Geometry
 import Geometry.Ray
 import Linear.Affine
-import Linear.Epsilon
 import Linear.Metric (norm, normalize)
 import Linear.V3
 import Linear.Vector ((^*))
@@ -15,19 +14,16 @@ data Sphere a = Sphere
   }
   deriving (Eq, Ord, Show)
 
--- | Compute the set of intersections between a Ray and a Sphere as a list of Intersections in increasing order of distance.
-intersections :: (Epsilon a, RealFloat a) => Sphere a -> Ray a -> [(a, Intersection a)]
-intersections (Sphere centre radius) (Ray origin direction) = if discriminant < 0 then [] else atDistance <$> filter (> 0) ts
-  where ts = [-b] <**> [(+), (-)] <*> [sqrt discriminant]
-        b = sum (direction * unP translated)
-        translated = origin - centre
-        discriminant = b ** 2 - norm (unP translated) ** 2 + radius ** 2
-        atDistance d = (d, Intersection intersection (normalize (unP (intersection - centre))))
-          where intersection = origin + P direction ^* d
-
-{-# SPECIALIZE Geometry.Sphere.intersections :: Sphere Double -> Ray Double -> [(Double, Intersection Double)] #-}
-{-# INLINABLE Geometry.Sphere.intersections #-}
 
 instance Geometry Sphere where
   origin = Geometry.Sphere.origin
-  intersections = Geometry.Sphere.intersections
+  intersections (Sphere centre radius) (Ray origin direction) = if discriminant < 0 then [] else atDistance <$> filter (> 0) ts
+    where ts = [-b] <**> [(+), (-)] <*> [sqrt discriminant]
+          b = sum (direction * unP translated)
+          translated = origin - centre
+          discriminant = b ** 2 - norm (unP translated) ** 2 + radius ** 2
+          atDistance d = (d, Intersection intersection (normalize (unP (intersection - centre))))
+            where intersection = origin + P direction ^* d
+
+  {-# SPECIALIZE intersections :: Sphere Double -> Ray Double -> [(Double, Intersection Double)] #-}
+  {-# INLINABLE intersections #-}
